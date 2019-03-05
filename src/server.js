77f9bca455fa9eda39fac18344d2163962d3ffff
shannon <shannonrmoranetz@gmail.com>
2019-03-05 02:34:11 -0700

    Update repo name and remote

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import "@babel/polyfill";
 import app from './app';
-app.set('port', process.env.PORT || 3001)
+app.set('port', process.env.PORT || 3001);
 
 app.listen(app.get('port'), () => {
   console.log(`Server running on port: ${app.get('port')}`)
